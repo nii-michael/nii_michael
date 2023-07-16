d19b1075f87bebe240d3e2b73100f29c8e994fe8
Mic <nii1999@protonmail.com>
2023-07-16 11:34:30 +0000

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: "4500",
-  },
-  base: "/my-portfolio/",
+  base: "/nii_michael/"
 });
