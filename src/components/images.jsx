484fb224c7821f817412aa4e8b111b5693a353ab
nii-michael <38083491+nii-michael@users.noreply.github.com>
2023-07-17 09:46:38 +0000

    Update images.jsx

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -2,38 +2,38 @@ import React from "react";
 
 
 export const WRImg = () => (
-  <img src="/my-portfolio/WR.png" alt="Weekly Report Dashboard screenshot" />
+  <img src="/nii_michael/WR.png" alt="Weekly Report Dashboard screenshot" />
 );
 export const MOImg = () => (
-  <img src="/my-portfolio/MO.png" alt="Mt. Olivet Methodist Church,Dansoman Website screenshot" />
+  <img src="/nii_michael/MO.png" alt="Mt. Olivet Methodist Church,Dansoman Website screenshot" />
 );
 export const NGImg = () => (
-  <img src="/my-portfolio/NG.png" alt="NEXTGEN Website Redesign screenshot" />
+  <img src="/nii_michael/NG.png" alt="NEXTGEN Website Redesign screenshot" />
 );
 export const EStoreImg = () => (
-  <img src="/my-portfolio/e-store.jpg" alt="e-Store screenshot" />
+  <img src="/nii_michael/e-store.jpg" alt="e-Store screenshot" />
 );
 export const TweeterImg = () => (
-  <img src="/my-portfolio/tweeter.jpg" alt="tweeter screenshot" />
+  <img src="/nii_michael/tweeter.jpg" alt="tweeter screenshot" />
 );
 export const BlogImg = () => (
-  <img src="/my-portfolio/blog.jpg" alt="blog" />
+  <img src="/nii_michael/blog.jpg" alt="blog" />
 );
 export const WindBnBImg = () => (
-  <img src="/my-portfolio/windbnb.jpg" alt="WindBnB screenshot" />
+  <img src="/nii_michael/windbnb.jpg" alt="WindBnB screenshot" />
 );
 export const ECImg = () => (
-  <img src="/my-portfolio/EC.PNG"alt="E-commerce website screenshot"/>
+  <img src="/nii_michael/EC.PNG"alt="E-commerce website screenshot"/>
 );
 export const CRImg = () => (
-  <img src="/my-portfolio/CR.PNG" alt="Car Rental Services website screenshot" />
+  <img src="/nii_michael/CR.PNG" alt="Car Rental Services website screenshot" />
 );
 export const COImg = () => (
-  <img src="/my-portfolio/CO.png" alt="Construction Website screenshot" />
+  <img src="/nii_michael/CO.png" alt="Construction Website screenshot" />
 );
 export const POImg = () => (
-  <img src="/my-portfolio/PO.PNG" alt="portfolio screenshot" />
+  <img src="/nii_michael/PO.PNG" alt="portfolio screenshot" />
 );
 export const SOImg = () => (
-  <img src="/my-portfolio/SO.PNG" alt="SHOPPIE screenshot" />
+  <img src="/nii_michael/SO.PNG" alt="SHOPPIE screenshot" />
 );
